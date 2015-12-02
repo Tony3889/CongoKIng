60d1b6fa9a62b93ba39613c9bc5fe67d2933954c
Kim Burns <student@nburns.local>
2015-12-02 17:35:08 -0600

    Initial Commit

---
+++ Bongo Blast! (final one)Tests/Bongo_Blast___final_one_Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Bongo_Blast___final_one_Tests.swift
+//  Bongo Blast! (final one)Tests
+//
+//  Created by nburns on 12/2/15.
+//  Copyright © 2015 nburns. All rights reserved.
+//
+
+import XCTest
+@testable import Bongo_Blast___final_one_
+
+class Bongo_Blast___final_one_Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
